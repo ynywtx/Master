5e1b7f580d8058ed5b03eda083dbba3dbe820151
Cintia Tian <mastercscintia@gmail.com>
2023-11-27 22:34:58 +0100

    u4a1

--- ScMeDES/Ubung4.py
+++ ScMeDES/Ubung4.py
@@ -1,19 +1,23 @@
 import simpy
 import numpy as np
-
 class MM1Queue:
     def __init__(self, env, arrival_rate, service_rate, initial_queue_length):
-        self.env = env
-        self.server = simpy.Resource(env, capacity=1)
-        self.arrival_rate = arrival_rate
-        self.service_rate = service_rate
-        self.queue = initial_queue_length
+            self.env = env
+            self.server = simpy.Resource(env, capacity=1)
+            self.arrival_rate = arrival_rate
+            self.service_rate = service_rate
+            self.queue = initial_queue_length
+            self.total_queue_length = 0
+            self.total_waiting_time = 0
+            self.num_departures = 0
+
 
     def arrival_process(self):
         while True:
             inter_arrival_time = np.random.exponential(1 / self.arrival_rate)
             yield self.env.timeout(inter_arrival_time)
             self.queue += 1
+            self.total_queue_length += self.queue
             print(f"Arrival at {self.env.now:.2f}. Queue length: {self.queue}")
 
     def service_process(self):
@@ -23,6 +27,8 @@ class MM1Queue:
                 service_time = np.random.exponential(1 / self.service_rate)
                 yield self.env.timeout(service_time)
                 self.queue -= 1
+                self.total_waiting_time += self.queue
+                self.num_departures += 1
                 print(f"Departure at {self.env.now:.2f}. Queue length: {self.queue}")
 
 def simulate_mm1_queue(arrival_rate, service_rate, initial_queue_length, sim_time):
@@ -34,10 +40,20 @@ def simulate_mm1_queue(arrival_rate, service_rate, initial_queue_length, sim_tim
 
     env.run(until=sim_time)
 
+    average_queue_length = mm1_queue.total_queue_length / sim_time
+    average_waiting_time = mm1_queue.total_waiting_time / mm1_queue.num_departures
+    lambda_ratio = average_queue_length / average_waiting_time
+
+    return lambda_ratio
+
 # Parameters
 arrival_rate = 0.5  # λ = 0.5Hz
 service_rate = 0.5  # μ = 0.5Hz
 initial_queue_length = 5
-simulation_time = 10
+simulation_time = 200
 
-simulate_mm1_queue(arrival_rate, service_rate, initial_queue_length, simulation_time)
+lambda_ratio = simulate_mm1_queue(arrival_rate, service_rate, initial_queue_length, simulation_time)
+print(f"lambda_ratio: {lambda_ratio}")
+
+
+
